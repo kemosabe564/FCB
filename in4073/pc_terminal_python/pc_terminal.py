7300b77eef10f89f87f9c328f81b1e97b13ad4ae
viviand0097 <viviand0097@gmail.com>
2021-06-07 13:06:01 +0200

    added p p1 to gui

--- in4073/pc_terminal_python/pc_terminal.py
+++ in4073/pc_terminal_python/pc_terminal.py
@@ -71,7 +71,7 @@ if __name__ == "__main__":
     controller = Controller(drone, joystick, keyboard)
 
     if not args.no_gui:
-        gui = GUI((800, 480), drone)
+        gui = GUI((800, 480), drone, controller)
 
     while running:
         if gui:
